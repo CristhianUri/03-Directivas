7849a5c47d16e4cf29b33346e1e786034e79af41
Cristhian Uriel <a3521110258@alumno.uttehuacan.edu.mx>
2022-09-29 23:28:47 -0500

    subiendo proyecto
    subiendo proyecto

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Directivas en Vue.js</h1>
+    <hr/>
+    <h2>v-text</h2>
+    <span v-text="'Hola desde la directiva v-text'"></span>
+    <br>
+    <span>Hola desde la directiva v-text</span>
+    <br>
+    <span v-text="titulo"></span>
+    <br>
+    Esta es una transmutacion de texto {{titulo}},con una variable props <br>
+    <span v-text="titulo"></span>
+    <br>
+    Hola hola 
+    <br>
+    
+  </div>
+</template>
+
+<style scoped>
+  div{
+    text-align: center;
+    margin: 0px 2rem;
+  }
+</style>
+<script>
+  export default{
+    data:()=>({
+      titulo:"Hola desde Vue.js",
+    }),
+  };
+</script>
